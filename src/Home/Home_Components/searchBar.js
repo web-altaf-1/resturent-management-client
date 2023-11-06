fefa52b170c3e3eb10b79c3b40af46400b8e73f4
justinsteinberg <justin.steinberg1@aol.com>
2023-11-06 02:08:05 -0500

    Adding search bar component on home page.

---
+++ src/Home/Home_Components/searchBar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Searching for: ${searchTerm}`);
+  };
+
+  const searchStyle = {
+    width: "100%",
+    padding: "10px",
+    fontSize: "16px",
+    border: "1px solid #ccc", 
+    borderRadius: "5px",
+  };
+
+  const searchBarForm = {
+    display: "flex",
+    alignItems: "center"
+  }
+
+  const buttonStyle = {
+    backgroundColor: '#007BFF',
+    color: 'white',
+    border: 'none',
+    padding: '12px 20px',
+    marginLeft: '0px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={{width: "50%"}}>
+      <form onSubmit={handleSearchSubmit} style={searchBarForm}>
+        <input
+          style={searchStyle}
+          type="text"
+          placeholder="Search for restaurants, businesses, etc."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button style={buttonStyle} type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,15 @@
+import React from "react";
+import SearchBar from "./Home_Components/searchBar";
+
 function Home() {
+    const homeStyle = {display: "flex", alignItems: "center", flexDirection: "column", backgroundColor: "#36454F", height: "60vh"};
     return (
-        <h1>Welcome to Restaurant Dev branch Sam version</h1>
+        <>
+            <div style={homeStyle}>
+                <h1>Welcome to Restaurant Dev branch Justin's Version</h1>
+                <SearchBar/>
+            </div>
+        </>
     )
 }
 
